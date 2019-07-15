86e75c54ed651ca2cf79c7ee568ebaf2f96688e6
刘子欣 <liuzixin@192.168.0.103>
2019-07-15 22:53:34 +0800

    add

--- 2.promise/6完整的promise/promise.js
+++ 2.promise/6完整的promise/promise.js
@@ -2,6 +2,43 @@ const PENDING = "PENDING";
 const FULFILLED = "FULFILLED";
 const REJECTED = "REJECTED";
 
+function resolvePromise(promise2,x,resolve,reject){
+    if(promise2 === x){
+        return reject(new TypeError('TypeError: Chaining cycle detected for promise #<Promise>'))
+    }
+
+    let called;
+    if(typeof x === 'function' || (typeof x === 'object' && x !== null)){
+        //是promise
+        try{
+            let then = x.then;
+            if(typeof then === 'function'){
+                //有then方法
+                then.call(x,y=>{
+                    if(called) return;
+                    called = true;
+                    resolvePromise(promise2,y,resolve,reject); 
+                },r=>{
+                    if(called) return;
+                    called = true;
+                    reject(r);
+                })
+            }else{
+                //没有then方法 不是promise 有可能是是个对象{then:null}
+                resolve(x);
+            }
+        }catch(e){
+            if(called) return;
+            called = true;
+            reject(e)
+        }
+    }else{
+        //不是promise
+        resolve(x)
+    }
+
+}
+
 
 class Promise {
     constructor(executor) {
@@ -10,15 +47,22 @@ class Promise {
         this.reason = undefined;
         this.onResolvedCallbacks = [];
         this.onRejectedCallbacks = [];
-        let resolve = (data) => {
-            this.value = data;
-            this.status = FULFILLED;
-            this.onResolvedCallbacks.forEach(fn => fn())
+        let resolve = (value) => {
+            if(value instanceof Promise){
+                return value.then(resolve,reject);
+            }
+            if(this.status === PENDING){
+                this.value = value;
+                this.status = FULFILLED;
+                this.onResolvedCallbacks.forEach(fn => fn())
+            }
         }
         let reject = (reason) => {
-            this.reason = reason;
-            this.status = REJECTED;
-            this.onRejectedCallbacks.forEach(fn => fn())
+            if(this.status === PENDING){
+                this.reason = reason;
+                this.status = REJECTED;
+                this.onRejectedCallbacks.forEach(fn => fn())
+            }
         }
         try{
             executor(resolve, reject)
@@ -29,23 +73,76 @@ class Promise {
     }
     then(onFulfilled, onRejected) {
 
-        if (this.status == FULFILLED) {
-            onFulfilled(this.value);
-        }
-        if (this.status == REJECTED) {
-            onRejected(this.reason);
-        }
+        onFulfilled = typeof onFulfilled==='function'?onFulfilled:val=>val;
+        onRejected = typeof onRejected==='function'?onRejected:err=>{throw err}
 
-        if (this.status == PENDING) {
-            this.onResolvedCallbacks.push(() => {
-                onFulfilled(this.value)
-            })
-            this.onRejectedCallbacks.push(() => {
-                onRejected(this.reason);
-            })
-        }
+        let promise2;
+        promise2 = new Promise((resolve,reject)=>{
+
+            if (this.status === FULFILLED) {
+                setTimeout(()=>{
+                    try{
+                        let x = onFulfilled(this.value);
+                        resolvePromise(promise2,x,resolve,reject);
+                    }catch(e){
+                        reject(e)
+                    }
+                })
+            }
+            if (this.status === REJECTED) {
+                setTimeout(()=>{
+                    try{
+                        let x = onRejected(this.reason);
+                        resolvePromise(promise2,x,resolve,reject);
+                    }catch(e){
+                        reject(e)
+                    }
+                })
+            }
+    
+            if (this.status === PENDING) {
+                this.onResolvedCallbacks.push(() => {
+                    setTimeout(()=>{
+                        try{
+                            let x = onFulfilled(this.value);
+                            resolvePromise(promise2,x,resolve,reject);
+                        }catch(e){
+                            reject(e)
+                        }
+                    })
+                })
+                this.onRejectedCallbacks.push(() => {
+                    setTimeout(()=>{
+                        try{
+                            let x = onRejected(this.reason);
+                            resolvePromise(promise2,x,resolve,reject);
+                        }catch(e){
+                            reject(e)
+                        }
+                    })
+                })
+            }
+        })
+        return promise2;
+
 
     }
+
+    catch(errCallback){
+        return this.then(null,errCallback);
+    }
 }
+
+
+// 希望测试一下这个库是否符合我们的promise A+规范
+// promises-aplus-tests
+Promise.defer = Promise.deferred = function(){
+    let dfd = {};
+    dfd.promise = new Promise((resolve,reject)=>{
+      dfd.resolve = resolve;
+      dfd.reject = reject;
+    });
+    return dfd;
+  }
 
 module.exports = Promise;
