60f0a7339701be310a81d3e655adb1d0a0746504
lzx <919111051@qq.com>
2019-07-31 17:58:41 +0800

    'add'

--- 10.fs/1.js
+++ 10.fs/1.js
@@ -1,12 +1,23 @@
-let fs= require('fs');
-let path = require('path')
 
-let resolve = (name)=>{
-    return path.resolve('__dirname',name)
- 
+let fs= require('fs');
+let path = require('path');
+
+let resolve = (filename)=>{
+    return path.resolve(__dirname,filename)
 }
 
-fs.open(resolve('a.txt'),'r',function (err,fd){ //fd 文件操作符
-    console.log(fd)
-  
+//读完再写
+// fs.readFile(resolve('a.txt'),function (err,data){ 
+    
+//     fs.writeFile(resolve('c.txt'),data,function(err){
+//         console.log('拷贝成功')
+//     })
+// }) 
+
+//边读边写
+fs.open(resolve('./a.txt'),'r',function (err,fd){
+    //读取 = 往内存中写入内容
+    //写入 = 就是从内存中拿出来内容
+
+    // fs.read(fd,)
 })
