d8c9687f48fc596146f734a8270991278a514775
lzx <919111051@qq.com>
2019-08-01 17:51:56 +0800

    'add'

--- 10.fs/1.js
+++ 10.fs/1.js
@@ -15,9 +15,33 @@ let resolve = (filename)=>{
 // }) 
 
 //边读边写
-fs.open(resolve('./a.txt'),'r',function (err,fd){
+fs.open(resolve('./a.txt'),'r',function (err,rfd){ //fd当前对应的这个文件的r操作
     //读取 = 往内存中写入内容
     //写入 = 就是从内存中拿出来内容
+    // console.log(fd) //fd window下面是3
+    fs.open(resolve('./c.txt'),'w',438,function (err,wfd){//0o666 权限 读:2 写:4 执行:1
+        let buffer = Buffer.alloc(3);
 
-    // fs.read(fd,)
+        let roffset = 0;
+        let woffset = 0;
+        //递归
+        let next = ()=>{
+            fs.read(rfd,buffer,0,3,roffset,function (err,bytesRead){ //bytesRead 真实读到的个数
+                if(bytesRead === 0){
+                    fs.close(rfd,()=>{});
+                    fs.close(wfd,()=>{});
+                    console.log('文件拷贝完毕');
+                }else{
+                    roffset += bytesRead
+                    fs.write(wfd,buffer,0,3,woffset,function (err,bytesWrite){
+                        woffset += bytesWrite;
+                        next()
+                    })  
+                }
+            })
+        }
+
+        next()
+    })
+    
 })
--- 10.fs/2.js-dir.js
+++ 10.fs/2.js-dir.js
@@ -1,3 +1,48 @@
 //fs
 // 文件操作 目录操作
-//
+
+
+const fs = require('fs');
+
+//同步创建
+let pathUrl = 'a/b/c/d/e';
+const mkdirSync = (pathUrl)=>{
+    let pathArr = pathUrl.split('/');
+
+    for(let i = 0; i<pathArr.length; i++){
+        let current = pathArr.slice(0,i+1).join('/');
+        try{
+            fs.accessSync(current) 
+        }catch(e){
+            fs.mkdirSync(current)
+        }
+    }
+}
+mkdirSync(pathUrl)
+
+//异步创建
+let pathUrl2 = ' c/b/c/d/e';
+const mkdir = (pathUrl,cb)=>{
+    let pathArr = pathUrl.split('/');
+    let next = (index)=>{
+        if(index === pathArr.length){
+            return cb();
+        }
+        let current = pathArr.slice(0,++index).join('/');
+        fs.access(current,function(err){
+            if(err){ //不能访问 报错 就要创建当前层
+                fs.mkdir(current,function (){
+                    next(index)
+                })
+            }else{//能访问 不报错 创建下一层
+                next(index)
+            }
+        })
+    }
+    next(0)
+
+}
+mkdir(pathUrl2,function (){
+    console.log('创建成功')
+})
+
