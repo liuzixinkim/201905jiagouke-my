d0918b62ad6b71e42b2757a07d766300243d3261
lzx <919111051@qq.com>
2019-08-08 17:59:52 +0800

    'add'

--- 11.stream/1.readStream.js
+++ 11.stream/1.readStream.js
@@ -1,13 +1,28 @@
+
 let fs = require('fs');
+
 let rs = fs.createReadStream('./a.txt',{
-    flags:'r',
-    encoding:null,//读取结果是buffer类型
-    autoClose:true,//读取完后自动关闭
+    flags:'r', // 操作
+    encoding:null,// 读取结果是buffer类型
+    autoClose:true,// 读取完后自动关闭
     start:0,
-    end:9,//包前包后
-    highWaterMark:64*1024
+    end:9,//包前又包后
+    highWaterMark:1 //每次读文件 读多少个 默认是64*1024 node中最小单位是字节
 });
-//默认叫非流动模式
+
+// console.log(rs)
+//默认叫非流动模式 需要监听个事件 rs.on('data',cb)
+let arr = [];
 rs.on('data',function (data){
-    console.log(data)
+    arr.push(data);
 })
+
+
+rs.on('end',function (){
+    console.log(Buffer.concat(arr).toString())
+  
+})
+
+
+
+
